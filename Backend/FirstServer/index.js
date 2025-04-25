a0ae9e24d36ed8057a421bbe8c6bb12e2ea64e1a
https://github.com/Mahalakshmi-Iyer/British-Airways-Dashboard.git <mahalakshmi.a.iyer@gmail.com>
2025-04-26 01:09:53 +0530

    Backend_250425

---
+++ Backend/FirstServer/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+
+const app = express();
+let port = 8080;
+
+// app.get("/", (req, res) => {
+//     res.send("You accessed the root path");
+// });
+// app.get("/about", (req, res) => {
+//     res.send("You accessed the about path");
+// });
+// app.get("/contact", (req, res) => {
+//     res.send("You accessed the contact path");
+// });
+// app.get('*', (req, res) => {
+//     res.send("Path doesn't exist");
+// });
+
+
+// app.get("/:username/:id", (req, res) => {
+//     let {username, id} = req.params;
+//     res.send(`hello @${username} ${id}`);
+// });
+
+app.get('/search', (req, res) => {
+    let {q} = req.query;
+    if(!q){
+        res.send("nothing searched");
+    }
+    res.send(`search results for query : ${q}`);
+});
+
+app.listen(port, () => {
+    console.log(`app listening on port ${port}`);
+    // app.use((req, res) => {
+    //     console.log("request received");
+
+    //     res.send("This is a basic response");
+    //     res.send({name:"Mahalakshmi Iyer", age:20});
+    //     res.send("<h1>Hello World! from Mahalakshmi Iyer</h1>");
+    // });
+});
